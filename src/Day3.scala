538d9434a392cb4464e799c380c1b7cde776128c
kaspar <kaspar@hellden.com>
2020-12-03 10:11:55 +0100

    day 3 improved

--- src/Day3.scala
+++ src/Day3.scala
@@ -2,26 +2,30 @@ import scala.annotation.tailrec
 
 object Day3 extends Base(3) {
 
-  private val grid = inputLines
-  private val tileWidth = inputLines.head.length
+  object Grid {
+    private val tile = inputLines
+    private val tileWidth = tile.head.length
+    val height: Int = tile.length
+    def isTree(x: Int, y: Int): Int =
+      if (tile(y)(x % tileWidth) == '#') 1 else 0
+  }
 
-  @tailrec
-  private def countTrees(x: Int, y: Int, dx: Int, dy: Int, trees: BigInt): BigInt =
-    if (y >= grid.length)
-      trees
-    else
-      countTrees(
-        x + dx, y + dy,
-        dx, dy,
-        trees + (if (grid(y)(x % tileWidth) == '#') 1 else 0))
-
-  private def countTrees(dx: Int, dy: Int): BigInt =
-    countTrees(0, 0, dx, dy, 0)
+  private def countTrees(dx: Int, dy: Int): BigInt = {
+    @tailrec
+    def countTrees(x: Int, y: Int, trees: BigInt): BigInt =
+      if (y >= Grid.height)
+        trees
+      else
+        countTrees(
+          x + dx, y + dy,
+          trees + Grid.isTree(x, y))
+    countTrees(0, 0, 0)
+  }
 
   override def part1: BigInt = // 228
     countTrees(3, 1)
 
-  override def part2: BigInt = // ???
+  override def part2: BigInt = // 6818112000
     countTrees(1, 1) *
       countTrees(3, 1) *
       countTrees(5, 1) *
