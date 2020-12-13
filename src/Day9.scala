6b2e0eb01d15f122d4794f0a808f9cc39c78f342
kaspar <kaspar@hellden.com>
2020-12-13 16:12:10 +0100

    solved day 9

---
+++ src/Day9.scala
@@ -0,0 +1,40 @@
+import scala.annotation.tailrec
+
+object Day9 extends Base(9) {
+
+  private val numbers = inputLines.map(_.toLong)
+
+  def isNotSum(n: Long, nums: List[Long]): Boolean =
+    {
+      for {
+        a <- nums
+        b <- nums
+        if a != b
+        if a + b == n
+      } yield (a, b)
+    }.isEmpty
+
+  @tailrec
+  def findSum(n: Long, nums: List[Long], acc: List[Long] = Nil): Option[Long] =
+    if (acc.sum == n)
+      Some(acc.min + acc.max)
+    else if (acc.sum > n || nums.isEmpty)
+      None
+    else
+      findSum(n, nums.tail, nums.head :: acc)
+
+  override def part1: Long = // 32321523
+    numbers
+      .sliding(26)
+      .collectFirst { case nums :+ n if isNotSum(n, nums) => n }
+      .head
+
+  override def part2: Long = { //
+    val n = part1
+    numbers
+      .indices
+      .view
+      .flatMap { index => findSum(n, numbers.drop(index)) }
+      .head
+  }
+}
